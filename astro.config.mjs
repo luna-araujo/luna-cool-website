336629ad6f69469399c3730d9604cc50c5a49114
--global <--global>
2026-01-22 00:14:39 -0300

    Update base path in Astro config for deployment to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://luna-araujo.github.io',
-  base: '/luna-cool-website',
+  base: '/',
   integrations: [tailwind()]
 });
