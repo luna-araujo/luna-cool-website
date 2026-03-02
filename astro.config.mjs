5ee09a87d420dfdc2e87e2fbb3281204dc44d9a0
Luna Araujo <luna_r_araujo@proton.me>
2026-03-02 00:05:17 -0300

    Enable dynamic game release page on Netlify SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: netlify(),
   site: 'https://luna-araujo.com',
   base: '/',
   image: {
